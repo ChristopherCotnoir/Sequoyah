f1d3587811d4f0c77d08464c4a3737f862b583ac
Quentin Headen <qheaden@phaseshiftsoftware.com>
2015-01-28 10:16:03 -0500

    Added a simple about page.

---
+++ app/views/pages/about.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.plain')
+@section('head-custom')
+<style>
+  .centered {
+    text-align: center;
+  }
+</style>
+@stop
+@section('content')
+<main>
+  <div class="centered">
+    <h2>About Sequoyah</h2>
+    <p>
+      Sequoyah is a syllabary creation tool focused on providing endangered language groups the ability to generate a written alphabet for their spoken language.
+    </p>
+  </div>
+</main>
+@stop
