cd3d16db5ef805821a8156969f1a059e7b3c5d6b
Quentin Headen <qheaden@phaseshiftsoftware.com>
2015-02-04 11:49:34 -0500

    Rewrote the syllabary page to dynamically generate the grid based on the vowel and consonant data.

--- app/controllers/SyllabaryController.php
+++ app/controllers/SyllabaryController.php
@@ -3,7 +3,13 @@
 class SyllabaryController extends BaseController
 {
 	public function ShowGrid()
-  {
-    return View::make('pages.syllabary'); 
+  {      
+    $vowels = array('a', 'e', 'i', 'o', 'u');
+    $consonants = array('b', 'd', 'f', 'g', 'h');
+    return View::make('pages.syllabary', array(
+      'vowels' => $vowels,
+      'consonants' => $consonants,
+      'starting_symbol' => 9985 // The starting wingdings symbol hex code in decimal.
+    ));
   }
 }
