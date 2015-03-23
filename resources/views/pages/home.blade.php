b96263a041843dd3719bbe93b1f3e6189ab10527
Preston Brown <prestonbrown@Prestons-MacBook-Pro-new.local>
2015-03-23 14:49:02 -0400

    Modified home screen

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.plain')
 @section('content')
-<main>
-Hello world!
-</main>
+
+<div class="homeContent">
+
+
+	<h1 id="strong-title">Sequoyah</h1>
+	
+	<h2 id = "title-description">
+		Sequoyah is a syllabary creation tool focused on providing endangered language groups the ability to generate a written 		alphabet for their spoken language.
+	</h2>
+
+</div>
+
+
+
+
+
 @stop
