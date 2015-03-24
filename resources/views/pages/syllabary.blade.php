57d9ea9cf8fd6cf64cf457c70e63c1d885ef7993
Preston Brown <prestonbrown@Prestons-MacBook-Pro-new.local>
2015-03-24 19:02:14 -0400

    Moved syllabary.blade.php CSS code into page.css, fixed issue where the td items would move on mouse hovers

--- resources/views/pages/syllabary.blade.php
+++ resources/views/pages/syllabary.blade.php
@@ -1,25 +1,6 @@
 @extends('layouts.plain')
 @section ('head-custom')
-<style type='text/css'>
-  #syllabaryGrid {
-    width: 100%;
-    font-size: 200%;
-  }
 
-  .headerCell {
-    background: #BDC3C7;
-  }
-
-  .headerCell svg {
-    width:100px;
-    height:100px;
-  }
-
-  .syllableCell svg {
-    width: 100px;
-    height: 100px;
-  }
-</style>
 @stop
 
 @section('content')
@@ -28,22 +9,27 @@
   <tr>
     <th class='headerCell'></th>
     @foreach($vowels as $vowel)
-      <th <?php echo 'class="headerCell" onmouseover="show(\'' . $vowel['ipa'] . '\')" onmouseout="hide(\'' . $vowel['ipa'] . '\')">'; ?><b>{{{ $vowel['ipa'] }}} <br /> <?php echo $vowel['symbol']; ?> <br /><div align="center" style="height:25px"> <?php echo '<button type="button" id="' . $vowel['ipa'] . '" style="display:none">Edit Symbol</button>'; ?></div></b></th>
+      <th <?php echo 'class="headerCell" onmouseover="show(\'' . $vowel['ipa'] . '\')" onmouseout="hide(\'' . $vowel['ipa'] . '\')">'; ?><b>{{{ $vowel['ipa'] }}} <br /> <?php echo $vowel['symbol']; ?> <br /><div align="center" style="height:25px"> <?php echo '<button type="button" class="edit-button" id="' . $vowel['ipa'] . '" style="display:none">Edit Symbol</button>'; ?></div></b></th>
       <?php $starting_symbol++; ?>
     @endforeach
   </tr>
 
   @foreach($consonants as $consonant)
     <tr>
-      <td <?php echo 'class="headerCell" onmouseover="show(\'' . $consonant['ipa'] . '\')" onmouseout="hide(\'' . $consonant['ipa'] . '\')">'; ?><b>{{{ $consonant['ipa'] }}}</b> <br /> <?php echo $consonant['symbol']; ?> <br /><div align="center" style="height:25px"> <?php echo '<button type="button" id="' . $consonant['ipa'] . '" style="display:none">Edit Symbol</button>'; ?></div></td>
+      <td <?php echo 'class="headerCell" onmouseover="show(\'' . $consonant['ipa'] . '\')" onmouseout="hide(\'' . $consonant['ipa'] . 			'\')">'; ?><b>{{{ $consonant['ipa'] }}}</b> <br /> <?php echo $consonant['symbol']; ?> <br /><div align="center" 			style="height:25px"> <?php echo '<button type="button" class="edit-button" id="' . $consonant['ipa'] . '" style="display:none">Edit 			Symbol</button>'; ?></div></td>
       <?php $starting_symbol++; ?>
       @for($i = 0; $i < count($vowels); $i++)
-        <td <?php echo 'class="syllableCell" onmouseover="show(\'' . $consonant['ipa'] . $vowels[$i]['ipa'] . '\')" onmouseout="hide(\'' . $consonant['ipa'] . $vowels[$i]['ipa'] . '\')">'; ?>{{{ $consonant['ipa'] . $vowels[$i]['ipa'] }}} <br />
-          <svg width="100%" height="100%">
+	  
+         <td <?php echo 'class="syllableCell" onmouseover="show(\'' . $consonant['ipa'] . $vowels[$i]['ipa'] . '\')" 			onmouseout="hide(\'' . $consonant['ipa'] . $vowels[$i]['ipa'] . '\')">'; ?>
+			{{{ $consonant['ipa'] . $vowels[$i]['ipa'] }}}
+			<br />
+          	<svg width="100%" height="100%">
             <?php echo $consonant['symbol'] . "\n" . $vowels[$i]['symbol'];?>
-			<br /><div align="center" style="height:25px"> <?php echo '<button type="button" id="' . $consonant['ipa'] . $vowels[$i]['ipa'] . '" style="display:none">Edit Symbol</button>'; ?></div>
+			<br /><div align="center" style="height:25px"> <?php echo '<button type="button" class="edit-button" id="' . $consonant['ipa'] . 			$vowels[$i]['ipa'] . '" style="display:none">Edit Symbol</button>'; ?></div>
           </svg>
-        </td>
+        </td> 
+			
+		
         <?php $starting_symbol++; ?>
       @endfor
     </tr>
