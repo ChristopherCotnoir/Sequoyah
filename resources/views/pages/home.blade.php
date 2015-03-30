3b89cb64e09566d18e9182fe547912926cdeabc4
Matt Hines <mhines@cs.odu.edu>
2015-03-30 14:51:49 -0400

    Added separate login section. Cleaned up page.css and cleaned up styling on home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,63 +2,32 @@
 @section('content')
 
 
-<div class="homeContent">
-	<div id="home-login">
-		<form id= "login-home" action='{{ action('Auth\AuthController@DoLogin') }}' method='post'>
-		<input type='hidden' id='csrf' name='_token' value='{{ csrf_token() }}'>
-			
-			<h1 id="white-text">Login <a href='' class='hint float-right'>(Forgot your password?)</a></h1><hr>
-	
-			
-			<label for='username' id = "white-text">Username</label>
-			@if ($errors->has('username'))
-				<output class='error'>{{ $errors->first('username') }}</output>
-			@endif
-			<input type='text' name='username' value='{{ old('username') }}'>
-	
-			
-			<label for='password' id = "white-text">Password</label>
-			@if ($errors->has('password'))
-				<output class='error'>{{ $errors->first('password') }}</output>
-			@endif
-			<input type='password' name='password' value=''>
-	
-			<div>
-				<label for='remember' id = "white-text">Remember Me?</label>
-				<input type='checkbox' name='remember' value='{{ old('remember') }}'>
-			</div>
-	
-			<input type='submit' value='Login'>
-		</form>
-		
+<div id='home-jumbo' class='space-bottom'>
+	<div class="about">
+		<h2>About Sequoyah</h2>
+		<p>Sequoyah is a syllabary creation tool focused on providing endangered language groups the ability to generate a written alphabet for their spoken language.</p>
 	</div>
+	@include('sections.auth.login', [ 'form_id' => 'login-home' ])
 </div>
-
-
-
-<br />
-<br />
-
-<div class="tiles">
+<div id="home-tiles">
 			<div class="tile">
 				<h2>About Sequoyah</h2>
-				<p>Sequoyah is a syllabary creation tool focused on providing endangered language groups the ability to
-			    		generate a written alphabet for their spoken language.</p>
+				<p>Sequoyah is a syllabary creation tool focused on providing endangered language groups the ability to generate a written alphabet for their spoken language.</p>
 			</div>
 			<div class="tile">
 				<h2>Feasibility</h2>
 					<p>
-				    	Based on the requirements of this project, it is certainly feasible. First, the requirements are the 						following:
+				    	Based on the requirements of this project, it is certainly feasible. First, the requirements are the following:
 					</p>
 		
 				    <ul>
 				    <li>
-						Allow the creation of a <a class='link' href='http://en.wikipedia.org/wiki/Syllabary'  						target="_blank">syllabary</a> for the language
+						Allow the creation of a <a class='link' href='http://en.wikipedia.org/wiki/Syllabary' target="_blank">syllabary</a> for the language
 					</li>
 				    <li>
 						Allow assigning custom characters to syllables of the language
 					</li>
-				    <li>Provide the ability to download a <a class='link' href='http://en.wikipedia.org/wiki/TrueType'  						target="_blank">TrueType font</a> of the generated written language to allow typing in the language
+				    <li>Provide the ability to download a <a class='link' href='http://en.wikipedia.org/wiki/TrueType' target="_blank">TrueType font</a> of the generated written language to allow typing in the language
 					</li> 
 				    </ul>
 
@@ -68,12 +37,8 @@
 			<div class="tile">
 				<h2>Competition</h2>
 				<p>
-			    	Based on our current research, we have found that this project is very unique. We have yet to find any projects 					that
-			    	are designed to generate written languages based on customized syllabaries.
+			    	Based on our current research, we have found that this project is very unique. We have yet to find any projects that are designed to generate written languages based on customized syllabaries.
 				</p>
 			</div>
 </div>
-
-
-
 @stop
