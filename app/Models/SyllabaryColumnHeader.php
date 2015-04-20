5a22eff79a82a2ff251b20fa286e24d7621e6298
Matt Hines <mhines@cs.odu.edu>
2015-04-20 18:09:18 -0400

    Added syllabary data to json output

--- app/Models/SyllabaryColumnHeader.php
+++ app/Models/SyllabaryColumnHeader.php
@@ -7,4 +7,9 @@ class SyllabaryColumnHeader extends Model {
 	protected $table = 'syllabary_column_header';
 
 	protected $fillable = ['syllabary_id', 'ipa', 'symbol_id', 'next_id', 'prev_id', 'audio_sample'];
+	
+	public function symbol()
+	{
+		return $this->hasOne('Sequoyah\Models\Symbol', 'id', 'symbol_id');
+	}
 }
--- app/Models/SyllabaryRowHeader.php
+++ app/Models/SyllabaryRowHeader.php
@@ -7,4 +7,9 @@ class SyllabaryRowHeader extends Model {
 	protected $table = 'syllabary_row_header';
 
 	protected $fillable = ['syllabary_id', 'ipa', 'symbol_id', 'next_id', 'prev_id', 'audio_sample'];
+	
+	public function symbol()
+	{
+		return $this->hasOne('Sequoyah\Models\Symbol', 'id', 'symbol_id');
+	}
 }
