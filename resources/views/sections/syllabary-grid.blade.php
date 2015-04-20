c4a5508749c212609b6381feaae00aea1a1aa258
Christopher Cotnoir <chriscotnoir@gmail.com>
2015-04-20 15:27:35 -0400

    Removed x button at the top of the modal windows. We don't need more than one exit button. Also made it so that the cells are un-highlighted when closing the modal window either through the close button or by clicking on a button that does something/will do something. Currently does not work when closing the modal window by clicking outside it.

--- resources/views/sections/syllabary-grid.blade.php
+++ resources/views/sections/syllabary-grid.blade.php
@@ -7,19 +7,18 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title">Edit Syllabary</h4>
             </div>
             <div class="modal-body">
                 
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addColumnLeft('-')">Add Column Left</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="removeSelectedColumn()">Remove Column</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addColumnRight('-')">Add Column Right</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal">Edit Vowel</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="editSymbol('{{{ $vowel['symbol_id'] 				}}}')">Edit Symbol</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addColumnLeft('-');unselectAll()">Add Column Left</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="removeSelectedColumn();unselectAll()">Remove Column</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addColumnRight('-');unselectAll()">Add Column Right</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="unselectAll()">Edit Vowel</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="editSymbol('{{{ $vowel['symbol_id'] 				}}}');unselectAll()">Edit Symbol</button>
             </div>
             <div class="modal-footer">
-                <button type="button" class="flat-button" data-dismiss="modal">Close</button>
+                <button type="button" class="flat-button" data-dismiss="modal" onclick="unselectAll()">Close</button>
             </div>
         </div>
     </div>
@@ -36,20 +35,19 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                 <h4 class="modal-title">Edit Syllabary</h4>
             </div>
             <div class="modal-body">
                 
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addRowTop('-')">Add Row Top</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="removeSelectedRow()">Remove Row</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addRowBottom('-')">Add Row 				Bottom</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal">Edit Consonant</button>
-				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="editSymbol('{{{ $consonant['symbol_id'] 				}}}')">Edit Symbol</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addRowTop('-');unselectAll()">Add Row Top</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="removeSelectedRow();unselectAll()">Remove Row</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="addRowBottom('-');unselectAll()">Add Row 				Bottom</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="unselectAll()">Edit Consonant</button>
+				<button type="button" class="btn btn-default" data-dismiss="modal" onclick="editSymbol('{{{ $consonant['symbol_id'] 				}}}');unselectAll()">Edit Symbol</button>
 				
             </div>
             <div class="modal-footer">
-                <button type="button" class="flat-button" data-dismiss="modal">Close</button>
+                <button type="button" class="flat-button" data-dismiss="modal" onclick="unselectAll()">Close</button>
             </div>
         </div>
     </div>
@@ -66,18 +64,17 @@
     	<div class="modal-dialog">
         	<div class="modal-content">
             	<div class="modal-header">
-                	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title">Edit Syllabary</h4>
 				</div>
 				<div class="modal-body">
                 
-					<button type="button" class="btn btn-default" data-dismiss="modal">Remove Cell</button>
-					<button type="button" class="btn btn-default" data-dismiss="modal">Restore Cell</button>
-					<button type="button" class="btn btn-default" data-dismiss="modal">Edit Symbol</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal" onclick="unselectAll()">Remove Cell</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal" onclick="unselectAll()">Restore Cell</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal" onclick="unselectAll()">Edit Symbol</button>
 				
 				</div>
 				<div class="modal-footer">
-					<button type="button" class="flat-button" data-dismiss="modal">Close</button>
+					<button type="button" class="flat-button" data-dismiss="modal" onclick="unselectAll()">Close</button>
 				</div>
 			</div>
 		</div>
