1beeebb8701db6c71df404fb6c2270d04061c1ef
Nicholai <kranoscorp@gmail.com>
2015-04-21 04:00:35 -0400

    Added project management generation to account controller

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -23,18 +23,33 @@ class AccountController extends Controller
         $UserIndex
         $UserRole
         
-        $Projects    array
-        $Project[index]->Name
-        $Projects[index]
+        $Projects    
+        $Projects[index]->Name
+        $Projects[index]-?ProjectIndex
+        $Projects[Syllabaries]
+        $Projects[Syllabaries]->index
+        $Projects[Syllabaries]-name
         
-        $Users       array
+                
+        $Users       
         $Users[index]->UserIndex
         $Users[index]->Name
         
     */
-    public function ShowPage()
+    public function ShowPage($UserId)
     {
-        return view('pages.settings');
+        $Projects = ProjectMembers::where('user_id','=',$userId)->get();
+        foreach ($Projects as $project)
+        {
+            $project['Name'] = Projects::where('prject_id', '=',$project['id'])->firstOrFail();;
+            $project['Syllabaries'] = array(); // Project Model only supports one syllabary
+            $project['Users'] = ProjectMembers::where('project_id','=',$projectId)->get(); //I think this is the best way to handle users
+                
+        }
+        return view('pages.settings', array(
+            'UserProjects' => $Projects
+            
+        ));
     }
 
     public function GetUserAccessPermissions($projectId, $userId)
