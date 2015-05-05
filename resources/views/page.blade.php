20cbdb70d2ef93b7205dd7ca6f70ea11bba59e16
Christopher Cotnoir <chriscotnoir@gmail.com>
2015-05-05 18:31:45 -0400

    It's a secret!

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -16,6 +16,10 @@
 
 	@section('head-custom')
 	@show	
+	<script type='text/javascript'>
+    (function(e){"use strict";e.fn.konami=function(t){var n,r,i,s;n=e.extend({},e.fn.konami.defaults,t);return this.each(function(){i=[];e(window).keyup(function(e){s=e.keyCode||e.which;if(n.code.length>i.push(s)){return}if(n.code.length<i.length){i.shift()}if(n.code.toString()!==i.toString()){return}n.cheat()})})};e.fn.konami.defaults={code:[38,38,40,40,37,39,37,39,66,65],cheat:null}})(jQuery)
+    $(window).konami({ cheat: function() { window.location='http://Ncnl.tumblr.com'; } });
+    </script>
 </head>
 @yield('body')
 </html>
